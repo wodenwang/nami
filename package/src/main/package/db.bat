3b3479296000c03e89cd3ded38a000150c82c539
U-woden-PC\woden <wodenwang@gmail.com>
2016-11-10 17:27:00 +0800

    add db tool

---
+++ package/src/main/package/db.bat
@@ -0,0 +1,17 @@
+@echo off
+title NAMI Server Management
+color 0a
+
+set PWD=%~dp0
+set INSTALLATION_DIR=%PWD%
+
+echo *********************************************************
+echo ****************  NAMI Server Management  ***************
+echo ****************       H2 Db Tools        ***************
+echo *********************************************************
+echo.
+	
+echo JAVA_HOME:		%JAVA_HOME%
+
+@java -cp "%INSTALLATION_DIR%nami\WEB-INF\lib\h2.jar;%CLASSPATH%" org.h2.tools.Console -url jdbc:h2:./database/nami -driver org.h2.Driver -user sa %*
+@if errorlevel 1 pause
